e1a6551e2fc19784d3f078b4d3a54fe7e15dcaa1
Hans Kristian W <hanskristianwijaya@yahoo.com>
2019-01-16 15:05:41 +0700

    fix tambahkan afektif DKN

--- laporan/laporan_lanjut_DKN.php
+++ laporan/laporan_lanjut_DKN.php
@@ -12,78 +12,96 @@
             $kelas_id = explode(",",$_POST['option_kelas']);
             $kelas_sem1 = $kelas_id[0];
             
+            mysqli_query($conn, "SET group_concat_max_len=15000"); 
+
             //laporan nilai akhir raport
             $query_dkn =
-                "SELECT urutan.siswa_no_induk, urutan.siswa_nama_depan, urutan.siswa_nama_belakang, urutan.mapel_nama, urutan.mapel_kkm, urutan.kelas_nama, for_kog, for_psi, sum_kog, sum_psi, mapel_persen_for, mapel_persen_sum, mapel_persen_for_psi, mapel_persen_sum_psi, mapel_persen_kog, mapel_persen_psi
-                 FROM 
-                (
-                    SELECT siswa_no_induk, siswa_nama_depan, siswa_nama_belakang, GROUP_CONCAT(mapel_nama_singkatan ORDER BY mapel_urutan) as mapel_nama, GROUP_CONCAT(mapel_kkm ORDER BY mapel_urutan) as mapel_kkm, kelas_nama
-                                FROM kelas
-                                LEFT JOIN d_mapel
-                                ON kelas_id = d_mapel_id_kelas
-                                LEFT JOIN mapel
-                                ON d_mapel_id_mapel = mapel_id
-                                LEFT JOIN siswa
-                                ON siswa_id_kelas = kelas_id
-                                WHERE kelas_id IN ($kelas_sem1)
-                                GROUP BY siswa_no_induk
-                                ORDER BY siswa_no_induk    
-                    
-                )as urutan
-                JOIN
-                (
-                    SELECT siswa_no_induk, siswa_nama_depan, GROUP_CONCAT(mapel_urutan ORDER BY mapel_urutan) as mapel_urutan, GROUP_CONCAT(mapel_id ORDER BY mapel_urutan) as mapel_id, GROUP_CONCAT(mapel_nama_singkatan ORDER BY mapel_urutan) as mapel_nama, GROUP_CONCAT(for_kog ORDER BY mapel_urutan) as for_kog, GROUP_CONCAT(for_psi ORDER BY mapel_urutan) as for_psi 
-                    FROM (
-                                SELECT siswa_no_induk, siswa_nama_depan, mapel_id, mapel_urutan, mapel_nama_singkatan,
-                                ROUND(SUM(ROUND(kog_quiz*kog_quiz_persen/100 + kog_ass*kog_ass_persen/100 + kog_test*kog_test_persen/100,0))/COUNT(DISTINCT kog_psi_topik_id),0)
-                                AS for_kog,
-                                ROUND(SUM(ROUND(psi_quiz*psi_quiz_persen/100 + psi_ass*psi_ass_persen/100 + psi_test*psi_test_persen/100,0))/COUNT(DISTINCT kog_psi_topik_id),0)
-                                AS for_psi
-                                FROM kog_psi 
-                                LEFT JOIN topik
-                                ON kog_psi_topik_id = topik_id
-                                LEFT JOIN mapel
-                                ON topik_mapel_id = mapel_id
-                                LEFT JOIN siswa
-                                ON kog_psi_siswa_id = siswa_id
-                                LEFT JOIN kelas
-                                ON siswa_id_kelas = kelas_id
-                                WHERE siswa_id_kelas = $kelas_sem1
-                                GROUP BY mapel_nama, kog_psi_siswa_id
-                                ORDER BY siswa_no_induk, mapel_urutan
-                    ) as formative
-                    GROUP BY siswa_no_induk
-                ) as formative_final ON urutan.siswa_no_induk = formative_final.siswa_no_induk
-                JOIN(
-                    SELECT siswa_no_induk, siswa_nama_depan,
-                    GROUP_CONCAT(mapel_id ORDER BY mapel_urutan) as mapel_id, 
-                    GROUP_CONCAT(mapel_urutan ORDER BY mapel_urutan) as mapel_urutan, 
-                    GROUP_CONCAT(mapel_nama_singkatan ORDER BY mapel_urutan) as mapel_nama_singkatan,
-                    GROUP_CONCAT(mapel_persen_for ORDER BY mapel_urutan) as mapel_persen_for,
-                    GROUP_CONCAT(mapel_persen_sum ORDER BY mapel_urutan) as mapel_persen_sum,
-                    GROUP_CONCAT(mapel_persen_for_psi ORDER BY mapel_urutan) as mapel_persen_for_psi,
-                    GROUP_CONCAT(mapel_persen_sum_psi ORDER BY mapel_urutan) as mapel_persen_sum_psi,
-                    GROUP_CONCAT(mapel_persen_kog ORDER BY mapel_urutan) as mapel_persen_kog,
-                    GROUP_CONCAT(mapel_persen_psi ORDER BY mapel_urutan) as mapel_persen_psi,
-                    GROUP_CONCAT(sum_kog ORDER BY mapel_urutan) as sum_kog,
-                    GROUP_CONCAT(sum_psi ORDER BY mapel_urutan) as sum_psi
-                    FROM(
-                
-                        SELECT siswa_no_induk, siswa_nama_depan, mapel_id, mapel_urutan, mapel_nama_singkatan, 
-                                                    mapel_persen_for, mapel_persen_sum, 
-                                                    mapel_persen_for_psi, mapel_persen_sum_psi,
-                                                    mapel_persen_kog, mapel_persen_psi,
-                                                    ROUND((kog_uts * kog_uts_persen + kog_uas * kog_uas_persen) /100,0) as sum_kog,
-                                                    ROUND((psi_uts * psi_uts_persen + psi_uas * psi_uas_persen) /100,0) as sum_psi
-                        FROM kog_psi_ujian
-                        LEFT JOIN mapel
-                        ON kog_psi_ujian_mapel_id = mapel_id
-                        LEFT JOIN siswa
-                        ON kog_psi_ujian_siswa_id = siswa_id
-                        WHERE siswa_id_kelas = $kelas_sem1
-                    )AS summative
-                    GROUP BY siswa_no_induk    
-                ) as summative_final ON urutan.siswa_no_induk = summative_final.siswa_no_induk";
+                'SELECT urutan.siswa_no_induk, urutan.siswa_nama_depan, urutan.siswa_nama_belakang, urutan.mapel_nama, urutan.mapel_kkm, urutan.kelas_nama, for_kog, for_psi, sum_kog, sum_psi, mapel_persen_for, mapel_persen_sum, mapel_persen_for_psi, mapel_persen_sum_psi, mapel_persen_kog, mapel_persen_psi, afektif_total, jumlah_bulan
+                FROM 
+               (
+                   SELECT siswa_no_induk, siswa_nama_depan, siswa_nama_belakang, GROUP_CONCAT(mapel_nama_singkatan ORDER BY mapel_urutan) as mapel_nama, GROUP_CONCAT(mapel_kkm ORDER BY mapel_urutan) as mapel_kkm, kelas_nama
+                               FROM kelas
+                               LEFT JOIN d_mapel
+                               ON kelas_id = d_mapel_id_kelas
+                               LEFT JOIN mapel
+                               ON d_mapel_id_mapel = mapel_id
+                               LEFT JOIN siswa
+                               ON siswa_id_kelas = kelas_id
+                               WHERE kelas_id IN ('.$kelas_sem1.')
+                               GROUP BY siswa_no_induk
+                               ORDER BY siswa_no_induk    
+                   
+               )as urutan
+               JOIN
+               (
+                   SELECT siswa_no_induk, siswa_nama_depan, GROUP_CONCAT(mapel_urutan ORDER BY mapel_urutan) as mapel_urutan, GROUP_CONCAT(mapel_id ORDER BY mapel_urutan) as mapel_id, GROUP_CONCAT(mapel_nama_singkatan ORDER BY mapel_urutan) as mapel_nama, GROUP_CONCAT(for_kog ORDER BY mapel_urutan) as for_kog, GROUP_CONCAT(for_psi ORDER BY mapel_urutan) as for_psi 
+                   FROM (
+                               SELECT siswa_no_induk, siswa_nama_depan, mapel_id, mapel_urutan, mapel_nama_singkatan,
+                               ROUND(SUM(ROUND(kog_quiz*kog_quiz_persen/100 + kog_ass*kog_ass_persen/100 + kog_test*kog_test_persen/100,0))/COUNT(DISTINCT kog_psi_topik_id),0)
+                               AS for_kog,
+                               ROUND(SUM(ROUND(psi_quiz*psi_quiz_persen/100 + psi_ass*psi_ass_persen/100 + psi_test*psi_test_persen/100,0))/COUNT(DISTINCT kog_psi_topik_id),0)
+                               AS for_psi
+                               FROM kog_psi 
+                               LEFT JOIN topik
+                               ON kog_psi_topik_id = topik_id
+                               LEFT JOIN mapel
+                               ON topik_mapel_id = mapel_id
+                               LEFT JOIN siswa
+                               ON kog_psi_siswa_id = siswa_id
+                               LEFT JOIN kelas
+                               ON siswa_id_kelas = kelas_id
+                               WHERE siswa_id_kelas = '.$kelas_sem1.'
+                               GROUP BY mapel_nama, kog_psi_siswa_id
+                               ORDER BY siswa_no_induk, mapel_urutan
+                   ) as formative
+                   GROUP BY siswa_no_induk
+               ) as formative_final ON urutan.siswa_no_induk = formative_final.siswa_no_induk
+               JOIN(
+                   SELECT siswa_no_induk, siswa_nama_depan,
+                   GROUP_CONCAT(mapel_id ORDER BY mapel_urutan) as mapel_id, 
+                   GROUP_CONCAT(mapel_urutan ORDER BY mapel_urutan) as mapel_urutan, 
+                   GROUP_CONCAT(mapel_nama_singkatan ORDER BY mapel_urutan) as mapel_nama_singkatan,
+                   GROUP_CONCAT(mapel_persen_for ORDER BY mapel_urutan) as mapel_persen_for,
+                   GROUP_CONCAT(mapel_persen_sum ORDER BY mapel_urutan) as mapel_persen_sum,
+                   GROUP_CONCAT(mapel_persen_for_psi ORDER BY mapel_urutan) as mapel_persen_for_psi,
+                   GROUP_CONCAT(mapel_persen_sum_psi ORDER BY mapel_urutan) as mapel_persen_sum_psi,
+                   GROUP_CONCAT(mapel_persen_kog ORDER BY mapel_urutan) as mapel_persen_kog,
+                   GROUP_CONCAT(mapel_persen_psi ORDER BY mapel_urutan) as mapel_persen_psi,
+                   GROUP_CONCAT(sum_kog ORDER BY mapel_urutan) as sum_kog,
+                   GROUP_CONCAT(sum_psi ORDER BY mapel_urutan) as sum_psi
+                   FROM(
+               
+                       SELECT siswa_no_induk, siswa_nama_depan, mapel_id, mapel_urutan, mapel_nama_singkatan, 
+                                                   mapel_persen_for, mapel_persen_sum, 
+                                                   mapel_persen_for_psi, mapel_persen_sum_psi,
+                                                   mapel_persen_kog, mapel_persen_psi,
+                                                   ROUND((kog_uts * kog_uts_persen + kog_uas * kog_uas_persen) /100,0) as sum_kog,
+                                                   ROUND((psi_uts * psi_uts_persen + psi_uas * psi_uas_persen) /100,0) as sum_psi
+                       FROM kog_psi_ujian
+                       LEFT JOIN mapel
+                       ON kog_psi_ujian_mapel_id = mapel_id
+                       LEFT JOIN siswa
+                       ON kog_psi_ujian_siswa_id = siswa_id
+                       WHERE siswa_id_kelas = '.$kelas_sem1.'
+                   )AS summative
+                   GROUP BY siswa_no_induk    
+               ) as summative_final ON urutan.siswa_no_induk = summative_final.siswa_no_induk
+               JOIN(
+                   SELECT siswa_no_induk, siswa_nama_depan, GROUP_CONCAT(mapel_id ORDER BY mapel_urutan) as mapel_id, GROUP_CONCAT(afektif_nilai ORDER BY mapel_urutan SEPARATOR ".") as afektif_total, GROUP_CONCAT(jumlah_bulan ORDER BY mapel_urutan) as jumlah_bulan
+                   FROM
+                   (
+                       SELECT siswa_no_induk, siswa_nama_depan, mapel_id, mapel_urutan, count(afektif_nilai) as jumlah_bulan, mapel_nama, GROUP_CONCAT(afektif_nilai ORDER BY mapel_urutan) as afektif_nilai
+                       FROM afektif
+                       LEFT JOIN mapel
+                       ON afektif_mapel_id = mapel_id
+                       LEFT JOIN siswa
+                       ON afektif_siswa_id = siswa_id
+                       WHERE siswa_id_kelas = '.$kelas_sem1.'
+                       GROUP BY siswa_no_induk, mapel_id
+                       ORDER BY siswa_nama_depan, mapel_urutan
+                   )AS afektif_total
+                   GROUP BY siswa_no_induk
+               )as afektif_final ON urutan.siswa_no_induk = afektif_final.siswa_no_induk';
 
 
             $query_dkn = mysqli_query($conn, $query_dkn);
@@ -104,6 +122,7 @@
             echo "<table class='rapot mt-3'>
                     <tr>
                         <th rowspan='2'>No</th>
+                        <th rowspan='2'>No Induk</th>
                         <th rowspan='2'>Nama Siswa</th>";
                 //nama mapel            
                 for($i=0;$i<count($nama_mapel);$i++){
@@ -129,6 +148,7 @@
                 }
                 echo "<tr>
                     <td style='padding: 0px 0px 0px 5px;'>$no</td>
+                    <td style='padding: 0px 0px 0px 5px;'>{$row2[0]}</td>
                     <td style='padding: 0px 0px 0px 5px;'>$nama_siswa</td>";
 
                 //kognitif (for_kog * mapel_persen_for + sum_kog * mapel_persen_sum)
@@ -144,6 +164,9 @@
                 //persentase
                 $mapel_persen_kog = explode(",",$row2['mapel_persen_kog']);
                 $mapel_persen_psi = explode(",",$row2['mapel_persen_psi']);
+                //afektif permapel
+                $afektif_mapel = explode(".",$row2['afektif_total']);
+                $jumlah_bulan = explode(",",$row2['jumlah_bulan']);
 
                 for($j=0;$j<count($nama_mapel);$j++){
                     $kognitif = round($for_kog[$j] * $mapel_persen_for[$j] + $sum_kog[$j] * $mapel_persen_sum[$j]);
@@ -154,7 +177,11 @@
                     //echo "<td style='text-align: center;'>".$for_psi[$j]."*".$mapel_persen_for_psi[$j]."+".$sum_psi[$j]."*".$mapel_persen_sum_psi[$j]."=".round($for_psi[$j] * $mapel_persen_for_psi[$j] + $sum_psi[$j] * $mapel_persen_sum_psi[$j])."</td>";
                     echo "<td style='text-align: center;'>".$psikomotor."</td>";
                     echo "<td style='text-align: center;'>".$n_akhir."</td>";
-                    echo "<td style='text-align: center;'>-</td>";
+
+                    //afektif minggu dipisahkan koma
+                    $nilai_perbulan = explode(',', $afektif_mapel[$j]);
+                    
+                    echo "<td style='text-align: center;'>".return_abjad_afek(return_total_nilai_afektif_bulan($nilai_perbulan)/$jumlah_bulan[$j])."</td>";
                 }
 
 
