fcebd7a18af9c997b329cc01057aebd9c6671c05
Hans Kristian <hanskristianwijaya@yahoo.com>
2019-02-09 22:40:47 +0700

    anti indihomo header footer

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="CSS/customCSS_preview.css">
     <title>CPA NSA</title>
+    <!-- </head> -->
 </head>
 <body>
     <!--##################START HERE###################-->
